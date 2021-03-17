61779ca75576b4d18f58cb379fbc96e8f5f3ae5a
Wolfkino <80814523+Wolfkino@users.noreply.github.com>
2021-03-17 10:07:26 +0200

    Add files via upload

---
+++ res/x5settings.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+|-------------------------------
+|	GENERAL SETTINGS
+|-------------------------------
+*/
+
+$imSettings['general'] = array(
+	'url' => 'http://ylas.com',
+	'public_folder' => '',
+	'salt' => 'mdr110975gjhm65ixw0kbn4a1zvxxvhkp61zv286o'
+);
+
+// End of file x5settings.php
